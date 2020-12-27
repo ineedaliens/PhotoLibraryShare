f356857e305d86dd2ebf2bb829a2e2ecb86bb1c9
Evgenij <68697277+ineedaliens@users.noreply.github.com>
2020-12-27 14:31:54 +0500

    Update FourthTableViewController.swift
    Delete Marks and Comment

--- PhotoLibraryShare/TableViewController/FourthTableViewController.swift
+++ PhotoLibraryShare/TableViewController/FourthTableViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 class FourthTableViewController: UITableViewController {
 
     
-    // MARK: - VAR, LET, ARRAY CONSTANT
+    
     var namefuture: [Namefuture] = []
     var spinner: UIActivityIndicatorView!
     
     
-    // MARK: - METHOD VIEW DID LOAD
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        spinner = UIActivityIndicatorView(style: .large) // задаем стиль ActivityIndicator
+        spinner = UIActivityIndicatorView(style: .large) 
         spinner.color = .black
-        spinner.translatesAutoresizingMaskIntoConstraints = false // свойство, которое не дает XCODE самостоятельно установить привязку элемента к экрану
-        spinner.hidesWhenStopped = true // свойство, которое прячет наш ActivityIndicator после того, как он закончил вращаться
-        spinner.startAnimating() // метод, анимация старта ActivityIndicator
-        tableView.addSubview(spinner) // добавляем наш ActivityIndicator в нащ TableView
+        spinner.translatesAutoresizingMaskIntoConstraints = false 
+        spinner.hidesWhenStopped = true 
+        spinner.startAnimating() 
+        tableView.addSubview(spinner) 
         
-        spinner.centerXAnchor.constraint(equalTo: tableView.centerXAnchor).isActive = true // добавляем констрейнт от ACtivityIndicator к нашему TableView и говорим,что он активный
+        spinner.centerXAnchor.constraint(equalTo: tableView.centerXAnchor).isActive = true 
         spinner.centerYAnchor.constraint(equalTo: tableView.centerYAnchor).isActive = true
         
         UIView.animate(withDuration: 0, delay: 10, options: .curveEaseIn, animations: {self.tableView.alpha = 1})
@@ -36,19 +36,19 @@ class FourthTableViewController: UITableViewController {
     }
 
     
-    // MARK: - TABLE METHOD NUMBER OF SECTIONS
+   
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     
-    // MARK: - TABLE METHOD NUMBER OF ROWS IN SECTION
+   
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return namefuture.count
     }
     
     
-    // MARK: - TABLE METHOD DID SELECT ROW AT
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
