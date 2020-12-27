0e0ec4ba2d7a7dba1e2d884d69c65023f5ce90ea
Evgenij <68697277+ineedaliens@users.noreply.github.com>
2020-12-27 14:18:04 +0500

    Update ThirdViewController.swift
    Delete Marks and Comment

--- PhotoLibraryShare/ViewController/ThirdViewController.swift
+++ PhotoLibraryShare/ViewController/ThirdViewController.swift
@@ -7,60 +7,60 @@
 
 import UIKit
 import MapKit
-// ПОСЛЕ СОЗДАНИЯ МОДЕЛИ COREDATA И ОПРЕДЕЛЕНИЯ ИЗОБРАЖЕНИЯ КАК DATA BINARY - ПО ПРОЕКТУ ПРИДЕТСЯ ИСПРАВЛЯТЬ ОШИБКИ. УДАЛЯТЬ СОЗДАННЫЙ ФАЙЛ СО СТРУКТУТОРОЙ НАШИХ СВОЙСТВ. ИЗОБРАЖЕНИЯ, КОТОРЫЕ ИМЕЕЮТ ВИД UIImage(named: namefuture[indexPath.row].images) НУЖНО ПРИВОДИТЬ К ТАКОМУ ВИДУ: UIImage(data: namefuture[indexPath.row].images! as Data), ТАК КАК НАШИ ИЗОБРАЖЕНИЯ ИМЕЮТ ФОРМАТ BINARY DATA
+
 class ThirdViewController: UIViewController, MKMapViewDelegate {
     
     
-    // MARK: - Table view data source
+   
     var namefutures: Namefuture!
     
     
-    // MARK: - Table view data source
+   
     @IBOutlet weak var mapView: MKMapView!
     
     
-    // MARK: - Table view data source
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self // наш класс будет реализовывать методы делегата!
+        mapView.delegate = self 
         
         
-        // MARK: - Table view data source
-        let geocoder = CLGeocoder() // позволяет из текста (адрес) вывести координаты
-        geocoder.geocodeAddressString(namefutures.location!, completionHandler: { (placemarks, error) in // метод, в который передаем нашу локацию и пишем кложер
-            guard error == nil else {return} // делаем проверку, если наша ошибка не нил то прерываем
-            guard let placemarks = placemarks else {return} // проверка
+        
+        let geocoder = CLGeocoder() 
+        geocoder.geocodeAddressString(namefutures.location!, completionHandler: { (placemarks, error) in 
+            guard error == nil else {return} 
+            guard let placemarks = placemarks else {return} 
             
-            let placemark = placemarks.first! // извлекаем из массива первый жлемент
+            let placemark = placemarks.first! 
             
-            let annotation = MKPointAnnotation() // делаем нотацию для нашей координаты
+            let annotation = MKPointAnnotation() 
             annotation.title = self.namefutures.name
             // annotation.subtitle = self.namefutures.type
             
-            guard let location = placemark.location else {return} // если такого адреса нет, то выполнение прерывается
-            annotation.coordinate = location.coordinate // если координаты доступны то мы присваиваем в нашу переменую
-            self.mapView.showAnnotations([annotation], animated: true) // вызываем метод, который принимает нашу анотацию в качестве координад, которые мы нашли
-            self.mapView.selectAnnotation(annotation, animated: true) // метод который разворачивает нашу аннотацию
+            guard let location = placemark.location else {return} 
+            annotation.coordinate = location.coordinate 
+            self.mapView.showAnnotations([annotation], animated: true) 
+            self.mapView.selectAnnotation(annotation, animated: true) 
         })
         
     }
     
     
-    // MARK: - Table view data source
+   
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard !(annotation is MKUserLocation) else {return nil} // проверка на наше местоположение
+        guard !(annotation is MKUserLocation) else {return nil} 
         
-        let annotationIdentifier = "restAnnotation" // Создаем идентификатор для метода, который снизу
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView// метод, который переиспользует нашу аннотацию. Аналогично работает как и ячейкой в тейблВью. И кастим наше переиспользованный идентификатор до класса ПИНАннотатионВью ( чтоб была видна иголочка и облачко с информацией!)
+        let annotationIdentifier = "restAnnotation" 
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-            annotationView?.canShowCallout = true // это свойство позволяет отображать дополнительную информацию о нашей аннотации
+            annotationView?.canShowCallout = true 
         }
-        let rightImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50)) // создали рамку, в которую будем помещшать нашу аннотацию
-        rightImage.image = UIImage(data: namefutures.images! as Data) // сюда мы передаем наше изображение из структуры
-        annotationView?.rightCalloutAccessoryView = rightImage // в правую часть аннотации мы помешаем нашу картинку
-        annotationView?.pinTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1) // делаем цвет иголки другим
-        return annotationView // возвращаем наш аннотатионвью
+        let rightImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50)) 
+        rightImage.image = UIImage(data: namefutures.images! as Data) 
+        annotationView?.rightCalloutAccessoryView = rightImage 
+        annotationView?.pinTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1) 
+        return annotationView 
     }
     
 }
